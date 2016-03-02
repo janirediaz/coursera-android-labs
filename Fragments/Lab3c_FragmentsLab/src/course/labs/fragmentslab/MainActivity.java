a90a03d3a045c0b404eef035b904d4b1432c0e02
JanireDiaz <janire.diaz@gmail.com>
2016-03-02 10:21:19 +0100

    Started the Lab2

--- Fragments/Lab3c_FragmentsLab/src/course/labs/fragmentslab/MainActivity.java
+++ Fragments/Lab3c_FragmentsLab/src/course/labs/fragmentslab/MainActivity.java
@@ -26,10 +26,10 @@ public class MainActivity extends Activity implements
 			mFriendsFragment = new FriendsFragment();
 
 			//TODO 1 - add the FriendsFragment to the fragment_container
+			FragmentTransaction transaction = getFragmentManager().beginTransaction();
+			transaction.add(R.id.fragment_container, mFriendsFragment);
+			transaction.commit();
 			
-			
-			
-
 		} else {
 
 			// Otherwise, save a reference to the FeedFragment for later use
